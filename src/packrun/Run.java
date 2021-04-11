ef1833df30bfee60d062d8c4b1632308293226b6
Hakermann420 <marcel.loebert@gmail.com>
2021-04-11 16:00:56 +0200

    different maps added (weather)

--- src/packrun/Run.java
+++ src/packrun/Run.java
@@ -25,7 +25,10 @@ public class Run{
 
 	private static float pauselen = 0.5f;
 	
-	private static LineMap linemap;
+	private static LineMap mapAvoidAll;
+	private static LineMap mapAvoidGelb;
+	private static LineMap mapAvoidGrün;
+	private static LineMap mapAvoidBlau;
 	
 	public static void main(String[] args) throws Throwable {	//throws Throwable
 		
@@ -33,41 +36,92 @@ public class Run{
 
 		/*******************************Map Stuff Beginnt*************************************/
 		
-		Line[] line = new Line[14];
+		Line[] lineall = new Line[14];
+		
+		Line[] linegelb = new Line[10];
+		Line[] linegrün = new Line[10];
+		Line[] lineblau = new Line[10];
 
 		//Gelbe Energie Nr 1
-		line[0] = new Line(271,9,683,337);
-		line[1] = new Line(271,337,683,9);
+		lineall[0] = new Line(271,9,683,337);
+		lineall[1] = new Line(271,337,683,9);
+
+		linegelb[0] = new Line(271,9,683,337);
+		linegelb[1] = new Line(271,337,683,9);
+
+		linegrün[0] = new Line(271,9,683,337);
+		linegrün[1] = new Line(271,337,683,9);
+		
+		lineblau[0] = new Line(271,9,683,337);
+		lineblau[1] = new Line(271,337,683,9);
 		
 		//Gelbe Energie Nr 2
-		line[2] = new Line(937,9,1347,337);
-		line[3] = new Line(937,337,1347,9);
+		lineall[2] = new Line(937,9,1347,337);
+		lineall[3] = new Line(937,337,1347,9);
+
+		linegelb[2] = new Line(937,9,1347,337);
+		linegelb[3] = new Line(937,337,1347,9);
+
+		linegrün[2] = new Line(937,9,1347,337);
+		linegrün[3] = new Line(937,337,1347,9);
+
+		lineblau[2] = new Line(937,9,1347,337);
+		lineblau[3] = new Line(937,337,1347,9);
 		
 		//Gelbe Zusatzenergie
-		line[4] = new Line(282,312,660,690);
-		line[5] = new Line(282,690,660,312);
+		lineall[4] = new Line(282,312,660,690);
+		lineall[5] = new Line(282,690,660,312);
+
+		linegelb[4] = new Line(282,312,660,690);
+		linegelb[5] = new Line(282,690,660,312);
 				
 		
 		//Grüne Energie
-		line[6] = new Line(626,756,1286,1115);
-		line[7] = new Line(626,1115,1286,756);
+		lineall[6] = new Line(626,756,1286,1115);
+		lineall[7] = new Line(626,1115,1286,756);
 
-		//Grüne Zusatzenergie
-		line[8] = new Line(1045,402,1482,779);
-		line[9] = new Line(1045,779,1482,402);
+		linegelb[6] = new Line(626,756,1286,1115);
+		linegelb[7] = new Line(626,1115,1286,756);
+
+		linegrün[4] = new Line(626,756,1286,1115);
+		linegrün[5] = new Line(626,1115,1286,756);
+
+		lineblau[4] = new Line(626,756,1286,1115);
+		lineblau[5] = new Line(626,1115,1286,756);
 		
+		//Grüne Zusatzenergie
+		lineall[8] = new Line(1045,402,1482,779);
+		lineall[9] = new Line(1045,779,1482,402);
+
+		linegrün[6] = new Line(1045,402,1482,779);
+		linegrün[7] = new Line(1045,779,1482,402);
 		
 		//Blaue Energie
-		line[10] = new Line(2069,172,2436,671);
-		line[11] = new Line(2069,671,2436,172);
+		lineall[10] = new Line(2069,172,2436,671);
+		lineall[11] = new Line(2069,671,2436,172);
+
+		linegelb[8] = new Line(2069,172,2436,671);
+		linegelb[9] = new Line(2069,671,2436,172);
+
+		linegrün[8] = new Line(2069,172,2436,671);
+		linegrün[9] = new Line(2069,671,2436,172);
+		
+		lineblau[6] = new Line(2069,172,2436,671);
+		lineblau[7] = new Line(2069,671,2436,172);
 
 		//Blaue Zusatzenergie
-		line[12] = new Line(1679,190,2056,567);
-		line[13] = new Line(1679,567,2056,190);
-				
+		lineall[12] = new Line(1679,190,2056,567);
+		lineall[13] = new Line(1679,567,2056,190);
+
+		lineblau[8] = new Line(1679,190,2056,567);
+		lineblau[9] = new Line(1679,567,2056,190);
 		
 		Rectangle rec = new Rectangle(0,0,2362,1143);
-		linemap = new LineMap(line, rec);
+		
+		mapAvoidAll = new LineMap(lineall, rec);
+		mapAvoidGelb = new LineMap(linegelb, rec);
+		mapAvoidGrün = new LineMap(linegrün, rec);
+		mapAvoidBlau = new LineMap(lineblau, rec);
 
 		/*******************************Map Stuff Vorbei*************************************/
 		
@@ -82,8 +136,26 @@ public class Run{
 		/********************************Fahrt Vorbei***************************************/
 	}
 	
-	public static LineMap getMap() {
-		return linemap;
+	/**
+	 * Returns the needed linemap according to the avoidNr parameter
+	 * @param avoidNr is the selector which map to return:
+	 * 			0: alle
+	 * 			1: gelb
+	 * 			2: grün
+	 * 			3: blau
+	 * @return the Linemap
+	 */
+	public static LineMap getMap(int avoidNr) {
+		switch(avoidNr) {
+			case 1:
+				return mapAvoidGelb;
+			case 2:
+				return mapAvoidGrün;
+			case 3:
+				return mapAvoidBlau;
+			default:
+				return mapAvoidAll;
+		}
 	}
 	
 	/**
